0d208435ce57a4aaa7c7b17d2a4026a045c162e4
bibossrou <bibossrou@gmail.com>
2026-04-30 00:08:12 +0200

    premier commit, cv2 lecture cam

---
+++ main.py
@@ -0,0 +1,30 @@
+import cv2
+import numpy as np
+import mediapipe as mp
+
+
+cam = cv2.VideoCapture(0)
+verif = True
+
+
+
+
+cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+cam.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+
+if not cam.isOpened():
+    print("Cam pas ouverte")
+    verif = False
+
+while verif:
+    ret, frame = cam.read()
+    #ret vérifie si frame existe et frame c'est l'image
+    frame_miroir = cv2.flip(frame, 1)
+    cv2.imshow('frame', frame_miroir)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+
+cam.release()
+cv2.destroyAllWindows()
