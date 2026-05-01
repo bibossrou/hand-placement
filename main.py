ace485b3e846f4bc6a16dfd29dd625d8983de153
bibossrou <bibossrou@gmail.com>
2026-05-01 22:55:14 +0200

    reglage probleme git

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import cv2
 import numpy as np
 import mediapipe as mp
+from ultralytics import YOLO
 
 cam = cv2.VideoCapture(0)
 verif = True
+
+model = YOLO("runs\\detect\\train-8\\weights\\best.pt")
 
 if not cam.isOpened():
     print("Cam pas ouverte")
@@ -13,7 +16,14 @@ while verif:
     ret, frame = cam.read()
     #ret vérifie si frame existe et frame c'est l'image
     frame_miroir = cv2.flip(frame, 1)
-    cv2.imshow('frame', frame_miroir)
+
+    
+    results = model(frame_miroir, stream=True)
+
+    for r in results:
+        annoted_frame = r.plot()
+
+    cv2.imshow('frame', annoted_frame)
 
     if cv2.waitKey(1) == ord('q'):
         break
--- modele_learning.py
+++ modele_learning.py
@@ -1,9 +1,24 @@
 from ultralytics import YOLO
+import multiprocessing
 
-model = YOLO("yolov8m.pt")
+def train_model():
+    model = YOLO("yolov8n.pt")
+    results = model.train(
+    data="Finger count.v2i.yolov8\data.yaml", 
+    epochs=50,
+    batch = 16,
+    lr0 = 0.001, 
+    imgsz=640,
+    mosaic=1.0,    
+    mixup=0.2,      
+    copy_paste=0.2,
+    degrees=20.0,   
+    scale=0.5,       
+    perspective=0.001, 
+    dropout=0.1,     
+    device=0   
+)
 
-
-model.info()
-
-
-results = model.train(data="fingers counting.v1i.yolov8\data.yaml", epochs=100, imgsz=640)
+if __name__ == '__main__':
+    multiprocessing.freeze_support()
+    train_model()
